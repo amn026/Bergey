d1695db5ec6282e5dd8d330ff78cb498c925bdcd
Albert Nguyen <amn026@ucsd.edu>
2016-10-01 19:01:01 -0700

    Added a working reset button.

--- Dilution/app/src/main/java/io/isawesome/albert/dilution/MainActivity.java
+++ Dilution/app/src/main/java/io/isawesome/albert/dilution/MainActivity.java
@@ -13,6 +13,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static java.lang.Double.parseDouble;
+
 public class MainActivity extends AppCompatActivity {
     EditText etC1;
     EditText etV1;
@@ -29,7 +31,10 @@ public class MainActivity extends AppCompatActivity {
     Spinner spinC2;
     Spinner spinV2;
 
-
+    double c1;
+    double v1;
+    double c2;
+    double v2;
 
     TextView tvAnswer;
 
@@ -44,16 +49,44 @@ public class MainActivity extends AppCompatActivity {
 
         ItemAdapter adapter = new ItemAdapter(this, a);
 
+        //Initialize boxes and spinners
         spinC1 = (Spinner)findViewById(R.id.spinC1);
         spinC1.setAdapter(adapter);
+        spinV1 = (Spinner)findViewById(R.id.spinV1);
+        spinC2 = (Spinner)findViewById(R.id.spinC2);
+        spinV2 = (Spinner)findViewById(R.id.spinV2);
 
         etC1 = (EditText)findViewById(R.id.etC1);
+        etV1 = (EditText)findViewById(R.id.etV1);
+        etC2 = (EditText)findViewById(R.id.etC2);
+        etV2 = (EditText)findViewById(R.id.etV2);
+
         tvAnswer = (TextView)findViewById(R.id.tvAnswer);
 
     }
 
     public void onSolve(View v) {
-        tvAnswer.setText(etC1.getText().toString());
+        double test;
+        String testString;
+
+        testString = etC1.getText().toString();
+
+        test = parseDouble(testString);
+        testString = String.valueOf(test);
+
+        tvAnswer.setText(testString);
+        //tvAnswer.setText(etC1.getText().toString());
         Log.d("lala",etC1.getText().toString());
+    }
+
+    public void onReset(View v) {
+        String resetString = "0";
+        //resetString = String.valueOf(resetString);
+        etC1.setText(resetString);
+        etV1.setText(resetString);
+        etC2.setText(resetString);
+        etV2.setText(resetString);
+
+        //tvAnswer.setText(" ", TextView.BufferType.NORMAL);
     }
 }
